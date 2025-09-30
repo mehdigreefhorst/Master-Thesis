d5db44b0d8ed8ec2a9595c037f8307e165b775b8
mehdigreefhorst <mehdigreefhorst@gmail.com>
2025-09-30 15:22:11 +0200

    update to the prep stage

--- app/database/entities/cluster_entity.py
+++ app/database/entities/cluster_entity.py
@@ -18,22 +18,28 @@ class ClusterTextThreadModeType(str, Enum):
     AppendedText = "appended_text"
     LlmParsedText = "llm_parsed_text"
 
+class ClusterPostPrepStatusType(str, Enum):
+    """explains the preparation from post to cluster units"""
+    Created = "created" # not started
+    Ongoing = "ongoing" # ongoing the prep
+    Completed = "completed" # finished the prep
+
 
 class ClusterEntity(BaseEntity):
     scraper_entity_id: PyObjectId
     text_thread_mode: ClusterStatusType # Literal["plain_text", "appended_text", "llm_parsed_text"]
     prompt: Optional[str] = None
     status: ClusterStatusType #Literal["initialized", "ongoing", "paused", "completed", "error"]
-    post_entity_ids_status: Dict[PyObjectId, bool]
+    post_entity_ids_prep_status: Dict[PyObjectId, ClusterPostPrepStatusType]
 
     @classmethod
     def from_params(cls, scraper_entity_id: PyObjectId, text_thread_mode: ClusterTextThreadModeType, post_entity_ids: List[PyObjectId], prompt: str = None) -> "ClusterEntity":
-        post_entity_ids_status = {post_entity_id: False for post_entity_id in post_entity_ids}
+        post_entity_ids_prep_status = {post_entity_id: ClusterPostPrepStatusType.Created for post_entity_id in post_entity_ids}
         return cls(
             scraper_entity_id=scraper_entity_id,
             text_thread_mode=text_thread_mode,
             prompt=prompt,
             status=ClusterStatusType.Initialized,
-            post_entity_ids_status= post_entity_ids_status
+            post_entity_ids_prep_status= post_entity_ids_prep_status
             
         )
--- app/services/cluster_prep_service.py
+++ app/services/cluster_prep_service.py
@@ -1,9 +1,10 @@
 
 
-from app.database.entities.cluster_entity import ClusterEntity, ClusterTextThreadModeType, ClusterStatusType
+from app.database.entities.base_entity import PyObjectId
+from app.database.entities.cluster_entity import ClusterEntity, ClusterPostPrepStatusType, ClusterTextThreadModeType, ClusterStatusType
 from app.database.entities.scraper_cluster_entity import ScraperClusterEntity
 from app.database.entities.scraper_entity import ScraperEntity
-from app.database import get_cluster_repository, get_cluster_unit_repository, get_scraper_cluster_repository
+from app.database import get_cluster_repository, get_cluster_unit_repository, get_post_repository, get_scraper_cluster_repository
 
 
 class ClusterPrepService:
@@ -22,7 +23,7 @@ class ClusterPrepService:
         cluster_entity = ClusterEntity.from_params(
             scraper_entity_id= scraping_entity.id,
             text_thread_mode= text_thread_mode,
-            post_entity_ids_status= post_entity_ids
+            post_entity_ids= post_entity_ids
         )
         get_cluster_repository().insert(cluster_entity)
         return cluster_entity
@@ -39,12 +40,33 @@ class ClusterPrepService:
         else:
             cluster_entity = get_cluster_repository().find_by_id(scraper_cluster_entity.cluster_entity_id)
             return cluster_entity
-        
-
 
     @staticmethod
-    def start_preparing_clustering(scraper_cluster_entity: ScraperClusterEntity):
-        cluster_entity = ClusterPrepService.get_or_create_cluster_entity(scraper_cluster_entity, prompt)
+    def start_preparing_clustering(scraper_cluster_entity: ScraperClusterEntity, prompt: str):
+        cluster_entity = ClusterPrepService.get_or_create_cluster_entity(scraper_cluster_entity)
+        cluster_entity.prompt = prompt
+        get_cluster_repository().update(cluster_entity.id, {"prompt": prompt})
+        for post_prep_status, post_id in cluster_entity.post_entity_ids_prep_status.items():
+            if post_prep_status == ClusterPostPrepStatusType.Created:
+                ClusterPrepService.convert_post_entity_to_cluster_units(cluster_entity, post_id)
+            elif post_prep_status == ClusterPostPrepStatusType.Ongoing:
+                # TODO what do we do here, it is probably not finished correctly?
+                raise Exception(f"We are with {cluster_entity.id} in a problem with post: {post_id} | we are ongoing???")
+            elif post_prep_status == ClusterPostPrepStatusType.Completed:
+                print(f"cluster_entity_id {cluster_entity.id} already finished with finished {post_id}")
+
+    @staticmethod
+    def convert_post_entity_to_cluster_units(cluster_entity: ClusterEntity, post_id: PyObjectId):
+        post_entity = get_post_repository().find_by_id(post_id)
+        # First we add the post as text by itself. since it is already valuable. Should we also add metadata of the replies/ comments?
+
+        # now we loop over each comment. and we send the post with commment to the LLM
+
+        # now we loop over the replies, this should send a function call to a new function with a recursive loop in it
+
+
+
+
 
 
         
