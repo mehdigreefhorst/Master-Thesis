a80485c46c9a2b050f8abfd152e6ba428b52355a
mehdigreefhorst <mehdigreefhorst@gmail.com>
2025-11-06 14:12:56 +0100

    prediction result

--- app/database/entities/experiment_entity.py
+++ app/database/entities/experiment_entity.py
@@ -1,10 +1,18 @@
 
 
 from enum import Enum
-from typing import Optional
+from typing import Dict, Optional
 
 from pydantic import BaseModel
 from app.database.entities.base_entity import BaseEntity, PyObjectId
+
+
+PrevalenceDict = Dict[int, int]
+
+
+class PredictionResult(BaseModel):
+    prevelance_dict: PrevalenceDict # e.g. {3: 120, 2: 40, 1: 10, 0: 100} -> First number is numbe of cluster units with the specific runs that have scored true  sum_ground_truth: int
+    sum_ground_truth: int
 
 
 class AggregateResult(BaseModel):
@@ -24,3 +32,4 @@ class ExperimentEntity(BaseEntity):
     sample_id: PyObjectId
     model: str
     aggregate_result: Optional[AggregateResult]
+    runs_per_unit: int = 3
