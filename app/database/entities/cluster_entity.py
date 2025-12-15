d111e3a072fa1644268f6aa9e451953c222619c8
mehdigreefhorst <mehdigreefhorst@gmail.com>
2025-12-15 09:22:11 +0100

    media strategy skip type added

--- app/database/entities/cluster_entity.py
+++ app/database/entities/cluster_entity.py
@@ -19,15 +19,16 @@ class ClusterEntity(BaseEntity):
     prompt_standalone_id: Optional[PyObjectId] = None # The final chosen prompt to evaluate all cluster units
     status: StatusType #Literal["initialized", "ongoing", "paused", "completed", "error"]
     post_entity_ids_prep_status: Dict[PyObjectId, StatusType]
-    media_strategy_skip_type: MediaStrategySkipType = MediaStrategySkipType.Ignore
+    media_strategy_skip_type: MediaStrategySkipType
 
     @classmethod
-    def from_params(cls, scraper_entity_id: PyObjectId, text_thread_mode: ClusterTextThreadModeType, post_entity_ids: List[PyObjectId], prompt: str = None) -> "ClusterEntity":
+    def from_params(cls, scraper_entity_id: PyObjectId, media_strategy_skip_type: MediaStrategySkipType, text_thread_mode: ClusterTextThreadModeType, post_entity_ids: List[PyObjectId], prompt: str = None) -> "ClusterEntity":
         post_entity_ids_prep_status = {post_entity_id: StatusType.Initialized for post_entity_id in post_entity_ids}
         return cls(
             scraper_entity_id=scraper_entity_id,
             text_thread_mode=text_thread_mode,
             prompt=prompt,
             status=StatusType.Initialized,
-            post_entity_ids_prep_status= post_entity_ids_prep_status
+            post_entity_ids_prep_status= post_entity_ids_prep_status,
+            media_strategy_skip_type=media_strategy_skip_type
         )
